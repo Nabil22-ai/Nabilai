412b14e62208d9a865ce7ba4645035e4671ee2d3
Nabil22-ai <elhairchabdenbi27@gmail.com>
2025-07-15 10:36:03 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: '50px', fontFamily: 'Arial' }}>
+      <h1>Dealings.io</h1>
+      <p>منصة بيع وشراء الدومينات</p>
+    </div>
+  );
+}
